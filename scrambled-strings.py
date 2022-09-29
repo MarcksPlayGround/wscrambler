c27c073567018c07be1164ab128f517bb2483915
MarcksPlayGround <mlaurence.c@gmail.com>
2022-09-29 13:20:18 +0800

    implemented a decorator to make my the code look cleaner.
    For the sake of the code looking a bit cleaner, i used a decorator.   Originally, implementing a "try-catch" was the original approach but i decided to try using a decorator to make it look more cleaner.

--- scrambled-strings.py
+++ scrambled-strings.py
@@ -1,61 +1,62 @@
 import hashlib
-import sys, getopt
+import sys, getopt, os
 import logging
 
+#decorator
+def failure(value):
+  def decorate(f):
+    def applicator(*args, **kwargs):
+      try:
+         return f(*args,**kwargs)
+      except Exception as e:
+         print(e)
+         logging.basicConfig(level=os.environ.get("LOGLEVEL", "CRITICAL"))
+
+    return applicator
+  return decorate
 
 
+@failure('')
+# function for checking the dictionary based on the limit
 def limit_check_dict(dictfilename):
-    try:
-        with open(dictfilename, "r") as f:
-            dictfile = [line.strip() for line in f] # get original string length
-            check_dict_size(dictfile) # limit: Check dictionary element and total size
-            check_dict_dup(dictfile) # limit: check duplicate element in list
-    except Exception as e:
-        print(e)
+    with open(dictfilename, "r") as f:
+        dictfile = [line.strip() for line in f] # get original string length
+    check_dict_size(dictfile) # limit: Check dictionary element and total size
+    check_dict_dup(dictfile) # limit: check duplicate element in list
     return dictfile
 
 
+@failure('')
 def limit_check_input(inputfilename):
-    try:
-        with open(inputfilename, "r") as d:
-            inputfile = [line.strip() for line in d]
-    except Exception as e:
-        print(e)
+    with open(inputfilename, "r") as d:
+        inputfile = [line.strip() for line in d]
     return inputfile
 
 # check the dictionary size
+@failure('')
 def check_dict_size(dictfile, minsize=2, maxsize = 105, totalmaxsize = 105):
-    try:
-        totaldictlen = len(''.join(dictfile))
-        if totaldictlen > totalmaxsize:
-            raise Exception("Greater than total maxsize of " + str(totalmaxsize))
-        for dicx in dictfile:
-            if len(dicx) < minsize:
-                raise Exception("dictionary lesser than " + str(minsize))
-            elif len(dicx) > maxsize:
-                raise Exception("greater than " + str(maxsize))
-    except Exception as e:
-        print(e)
-        sys.exit(1)
+    totaldictlen = len(''.join(dictfile))
+    if totaldictlen > totalmaxsize:
+        raise Exception("Greater than total maxsize of " + str(totalmaxsize))
+    for dicx in dictfile:
+        if len(dicx) < minsize:
+            raise Exception("dictionary lesser than " + str(minsize))
+        elif len(dicx) > maxsize:
+            raise Exception("greater than " + str(maxsize))
 
 #check for duplicate in dictionary
+@failure('')
 def check_dict_dup(dictfile):
-    try:
-        if len(dictfile) != len(set(dictfile)):
-            raise Exception("There is a duplicate in the list")
-    except Exception as e:
-        print(e)
-        sys.exit(1)
+    if len(dictfile) != len(set(dictfile)):
+        raise Exception("There is a duplicate in the list")
 
 
 
 if __name__ == '__main__':
 
     argumentList = sys.argv[1:]
-    # Options
     options = ""
-    # Long options
-    long_options = ["dictionary=", "input="]
+    long_options = ["dictionary=", "input="] # specify the options for the argument input
     try:
         # Parsing argument
         arguments, values = getopt.getopt(argumentList, options, long_options)
@@ -68,34 +69,29 @@ if __name__ == '__main__':
     except getopt.error as err:
         # output error, and return with an error code
         print(str(err))
-
+        logging.basicConfig(level=os.environ.get("LOGLEVEL", "CRITICAL"))
     try:
-        dictfile = limit_check_dict(var_dictfile)
-        inputfile =limit_check_input(var_inputfile)
+        dictfile = limit_check_dict(var_dictfile) #validate the dictionary based on the "limit".
+        inputfile =limit_check_input(var_inputfile) #validate the input file based on the "limit"
 
         casecntr = 0
         for inputstr in inputfile:
-            casecntr += 1
-            dupcntr = 0
+            casecntr += 1 # case number counter
+            dupcntr = 0 # duplicate counter.  show number of times the dictionary value appeared
 
             for testc in dictfile:
                 tmplist = []
-                testclen = len(testc)
+                testclen = len(testc) # get the length of the dictionary word element
 
-                for y, ycntr in enumerate(inputstr):
-                    # get the length of the testing string
-                    # sorted testc prepared for hashing
-                    testcsorted = ''.join(sorted(testc))
-                    # main hash.  hash the sorted string.
-                    mainhashedtest = hashlib.md5(testcsorted.encode('utf-8')).hexdigest()
-                    # get the string with the same length as the one in the dictfile(list)
-                    sstr = inputstr[y:y + testclen]
-                    # a string with the same first and last char was found
-                    if sstr[0] == testc[0] and testc[len(sstr) - 1] == sstr[len(sstr) - 1]:
-                        # sort the string and hash. if its equal to the main hash, then they are the same
-                        sortedsubstrhash = hashlib.md5(''.join(sorted(sstr)).encode('utf-8')).hexdigest()
+                for yvar, ycntr in enumerate(inputstr): #enumerate through each of the character in the input file
+                    testcsorted = ''.join(sorted(testc)) #before hashing, sort into alphabetical order the dictionary word/element
+                    mainhashedtest = hashlib.md5(testcsorted.encode('utf-8')).hexdigest()  # main hash.  hash the sorted dictionary word/element
+                    sstr = inputstr[yvar:yvar + testclen] #from the current position in the list, get the word based on the length of the dictionary word/element
 
-                        # check if the same value as the main hash.  if true, then they are the same string. if true, store the original unsorted string and store in the tmplist so we can check later if it already occured.
+                    if sstr[0] == testc[0] and testc[len(sstr) - 1] == sstr[len(sstr) - 1]: # a string with the same first and last char was found
+                        sortedsubstrhash = hashlib.md5(''.join(sorted(sstr)).encode('utf-8')).hexdigest() # sort the string and hash. if its equal to the main hash, then they are the same
+                        # check if the same value as the main hash.  if true, then they are the same string.
+                        # if true, store the original unsorted string and store in the tmplist so we can check later if it already occured.
                         if sortedsubstrhash == mainhashedtest:
                             # check first if this string already exist on the list
                             if sstr not in tmplist:
@@ -105,5 +101,5 @@ if __name__ == '__main__':
 
     except Exception as e:
         print(e)
-
+        logging.basicConfig(level=os.environ.get("LOGLEVEL", "CRITICAL"))
 
