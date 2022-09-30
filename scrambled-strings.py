baf7d0002dbccf6c5b13bec0a6c6af785987c49a
MarcksPlayGround <mlaurence.c@gmail.com>
2022-09-30 10:05:40 +0800

    updated comments and variable names.

--- scrambled-strings.py
+++ scrambled-strings.py
@@ -11,14 +11,13 @@ def failure(value):
       except Exception as e:
          print(e)
          logging.basicConfig(level=os.environ.get("LOGLEVEL", "CRITICAL"))
-
     return applicator
   return decorate
 
 
 @failure('')
-# function for checking the dictionary based on the limit
-def limit_check_dict(dictfilename):
+# validator: check the dictionary based on limit
+def validator_check_dict(dictfilename):
     with open(dictfilename, "r") as f:
         dictfile = [line.strip() for line in f] # get original string length
     check_dict_size(dictfile) # limit: Check dictionary element and total size
@@ -27,7 +26,7 @@ def limit_check_dict(dictfilename):
 
 
 @failure('')
-def limit_check_input(inputfilename):
+def validator_check_input(inputfilename):
     with open(inputfilename, "r") as d:
         inputfile = [line.strip() for line in d]
     return inputfile
@@ -51,7 +50,6 @@ def check_dict_dup(dictfile):
         raise Exception("There is a duplicate in the list")
 
 
-
 if __name__ == '__main__':
 
     argumentList = sys.argv[1:]
@@ -71,32 +69,34 @@ if __name__ == '__main__':
         print(str(err))
         logging.basicConfig(level=os.environ.get("LOGLEVEL", "CRITICAL"))
     try:
-        dictfile = limit_check_dict(var_dictfile) #validate the dictionary based on the "limit".
-        inputfile =limit_check_input(var_inputfile) #validate the input file based on the "limit"
+        dictfile = validator_check_dict(var_dictfile) #validate the dictionary based on the "limit". will return a list
+        inputfile = validator_check_input(var_inputfile) #validate the input file based on the "limit". will return a list
 
         casecntr = 0
-        for inputstr in inputfile:
+        for inputstr in inputfile: # loop for each line in input file
             casecntr += 1 # case number counter
             dupcntr = 0 # duplicate counter.  show number of times the dictionary value appeared
 
-            for testc in dictfile:
-                tmplist = []
-                testclen = len(testc) # get the length of the dictionary word element
+            for dict_word in dictfile: #loop for each element in dictionary
+                tmplist = [] #temporary list to store hashed words that already occured.
+                dict_word_len = len(dict_word) # get the length of the dictionary word
 
-                for yvar, ycntr in enumerate(inputstr): #enumerate through each of the character in the input file
-                    testcsorted = ''.join(sorted(testc)) #before hashing, sort into alphabetical order the dictionary word/element
-                    mainhashedtest = hashlib.md5(testcsorted.encode('utf-8')).hexdigest()  # main hash.  hash the sorted dictionary word/element
-                    sstr = inputstr[yvar:yvar + testclen] #from the current position in the list, get the word based on the length of the dictionary word/element
+                for yvar, ycntr in enumerate(inputstr): #enumerate through each of the character in the input file string
+                    dict_word_sorted = ''.join(sorted(dict_word)) #dict_word_sorted is a variable for the sorted dictionary word.
+                    mainhashedtest = hashlib.md5(dict_word_sorted.encode('utf-8')).hexdigest()  # mainhashedtest  hash the sorted dictionary word/element. (dict_word_sorted)
 
-                    if sstr[0] == testc[0] and testc[len(sstr) - 1] == sstr[len(sstr) - 1]: # a string with the same first and last char was found
-                        sortedsubstrhash = hashlib.md5(''.join(sorted(sstr)).encode('utf-8')).hexdigest() # sort the string and hash. if its equal to the main hash, then they are the same
+                    input_substr = inputstr[yvar:yvar + dict_word_len] #retrieve the substring from the "input" string starting from the current position up to the length of the dictionary word.
+
+                    #input_substr is the substring.  dict_word is the dictionary word being checked
+                    if input_substr[0] == dict_word[0] and dict_word[len(input_substr) - 1] == input_substr[len(input_substr) - 1]: # compare if the  dictionary words first and last character is the same as the "input" strings' first and last character.
+                        sortedsubstrhash = hashlib.md5(''.join(sorted(input_substr)).encode('utf-8')).hexdigest() # sort the string and hash. if its equal to the main hash, then they are the same
                         # check if the same value as the main hash.  if true, then they are the same string.
-                        # if true, store the original unsorted string and store in the tmplist so we can check later if it already occured.
                         if sortedsubstrhash == mainhashedtest:
-                            # check first if this string already exist on the list
-                            if sstr not in tmplist:
+                            # check first if this string already exist on the list  #if the input_substr already occured before do not count it and do not append in the tmplist.
+                            if input_substr not in tmplist:
                                 dupcntr += 1
-                                tmplist.append(sstr)
+                                tmplist.append(input_substr)  # if true, store the original unsorted string and store in the tmplist so we can check later if it already occured.
+
             print("case #" + str(casecntr) + ": " + str(dupcntr) )
 
     except Exception as e:
